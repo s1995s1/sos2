2d0e8d14be941d2a78dbd0314604d09f5c3cc52d
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-26 16:56:30 +0300

    СОХ

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let txt = ''
-for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`
-m.reply(`*𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙴𝙽 𝙻𝙾𝚂 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰 𝙴𝙻 𝙱𝙾𝚃:*
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? 'НЕ УЧАСНИК' : 'УЧАСНИК'}]\n\n`
+m.reply(`*СПИСОК ГРУПП, В КОТОРЫХ НАХОДИТСЯ БОТ:*
 ${txt}
 `.trim())
 }
