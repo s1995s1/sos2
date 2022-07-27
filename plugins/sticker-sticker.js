58a68f814607273c05bee587296b21a4f608daaa
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-27 12:08:04 +0300

    лп

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -43,7 +43,7 @@ else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾�
 
 }}
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
-handler.tags = ['sticker']
+handler.tags = ['стикер']
 handler.command = /^s(sti?ker??(gif)?(wm)?$/i
 export default handler
 
