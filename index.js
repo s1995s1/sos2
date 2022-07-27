1a43316318ef3b07fbc4cecd212cc1cc5684be02
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-27 17:16:05 +0300

    шжгпр

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('NotJhuz-MD\nJhusi-Jaashi', {
+say('Бот Царя\загрузка-ожидайте', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Jhus. Dedicado para Jaashi, si lees esto, te amo uwu`, {
+say(`Ураа я увидел белый свет своими глазами, Создатель,Ты красафчек`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
