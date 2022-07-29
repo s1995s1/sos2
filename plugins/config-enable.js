78b07972b8a01ff250542cffe8818ea65a3c688b
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-29 03:08:33 +0300

    вепрь

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -3,54 +3,53 @@ const sections = [
 {
 title: `𝐋𝐈𝐒𝐓𝐀 𝐃𝐄 𝐎𝐏𝐂𝐈𝐎𝐍𝐄𝐒`,
 rows: [
-{title: "✨ | 𝚆𝙴𝙻𝙲𝙾𝙼𝙴", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰 𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙰 𝙴𝙽 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾", rowId: `${usedPrefix + command} welcome`},
-{title: "🌎 | 𝙼𝙾𝙳𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚅𝚄𝙴𝙻𝚅𝙴 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 𝚈/𝙾 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix + command} public`},
-{title: "🔗 | 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿", rowId: `${usedPrefix + command} antilink`},   
-{title: "🔗 | 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝟸", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝚀𝚄𝙴 𝙸𝙽𝙸𝙲𝙸𝙰𝙽 𝙴𝙽 𝙷𝚃𝚃𝙿𝚂", rowId: `${usedPrefix + command} antilink2`},    
-{title: "🔎 | 𝙳𝙴𝚃𝙴𝙲𝚃", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝙽𝙾𝚃𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽𝙴𝚂 𝙳𝙴 𝙽𝚄𝙴𝚅𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽 𝙴𝙽 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾", rowId: `${usedPrefix + command} detect`},      
-{title: "❗ | 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝚃", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 𝙿𝙰𝚁𝙰 𝚂𝙰𝙲𝙰𝚁 𝙶𝙴𝙽𝚃𝙴 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂", rowId: `${usedPrefix + command} restrict`},    
-{title: "☑️ | 𝙰𝚄𝚃𝙾𝚁𝙴𝙰𝙳", description: "𝙼𝙰𝚁𝙲𝙰 𝙰𝚄𝚃𝙾𝙼𝙰𝚃𝙸𝙲𝙰𝙼𝙴𝙽𝚃𝙴 𝙻𝙰𝚂 𝙲𝙾𝙽𝚅𝙴𝚁𝚂𝙰𝙲𝙸𝙾𝙽𝙴𝚂 𝙲𝙾𝙼𝙾 𝙻𝙴𝙸𝙳𝙾", rowId: `${usedPrefix + command} autoread`},
-{title: "🔊 | 𝙰𝚄𝙳𝙸𝙾𝚂", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝙳𝙴 𝙰𝚄𝙳𝙸𝙾𝚂 𝚂𝙸𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾", rowId: `${usedPrefix + command} audios`},
-{title: "👾 | 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁", description: "𝚃𝙾𝙳𝙰𝚂 𝙻𝙰𝚂 𝙸𝙼𝙰𝙶𝙴𝙽𝙴𝚂, 𝚅𝙸𝙳𝙴𝙾𝚂 𝙾 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝙴𝙽𝚅𝙸𝙰𝙳𝙾𝚂 𝚂𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝙽 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁", rowId: `${usedPrefix + command} autosticker`},
-{title: "💬 | 𝙿𝙲𝙾𝙽𝙻𝚈", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙾𝙻𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁𝙰 𝙰 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚂𝙸 𝙴𝚂 𝚄𝙽 𝙲𝙷𝙰𝚃 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix + command} pconly`},
-{title: "🏢 | 𝙶𝙲𝙾𝙽𝙻𝚈", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙾𝙻𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁𝙰 𝙰 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚂𝙸 𝙴𝚂 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾", rowId: `${usedPrefix + command} gconly`},
-]}, ]
+{title: "✨ | ПРИВЕТСТВИЕ", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰 𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙰 𝙴𝙽 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾", rowId: `${usedPrefix + command} welcome`},
+{title: "🌎 | ОБЩЕДОСТУПНЫЙ РЕЖИМ", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚅𝚄𝙴𝙻𝚅𝙴 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 𝚈/𝙾 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `${usedPrefix + command} public`},
+{title: "🔗 | АНТИССЫЛКА", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿", rowId: `${usedPrefix + command} antilink`},   
+{title: "🔗 | АНТИССЫЛКА 2", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝚀𝚄𝙴 𝙸𝙽𝙸𝙲𝙸𝙰𝙽 𝙴𝙽 𝙷𝚃𝚃𝙿𝚂", rowId: `${usedPrefix + command} antilink2`},    
+{title: "🔎 | ОБНАРУЖИВАТЬ", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝙽𝙾𝚃𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽𝙴𝚂 𝙳𝙴 𝙽𝚄𝙴𝚅𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽 𝙴𝙽 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾", rowId: `${usedPrefix + command} detect`},      
+{title: "❗   | ОГРАНИЧИВАТЬ", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 𝙿𝙰𝚁𝙰 𝚂𝙰𝙲𝙰𝚁 𝙶𝙴𝙽𝚃𝙴 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂", rowId: `${usedPrefix + command} restrict`},    
+{title: "☑️ | АВТОЧТЕНИЕ", description: "АВТОМАТИЧЕСКОЕ ПОМЕТКА БЕСЕД КАК ПРОЧИТАННЫХ", rowId: `${usedPrefix + command} autoread`},
+{title: "🔊 | АУДИО", description: "ВКЛЮЧЕНИЕ И ОТКЛЮЧЕНИЕ НЕПРЕДВИДЕННЫХ ЗВУКОВЫХ КОМАНД", rowId: `${usedPrefix + command} audios`},
+{title: "👾 | АВТОСТИКЕР", description: "ВСЕ ОТПРАВЛЕННЫЕ ИЗОБРАЖЕНИЯ, ВИДЕО ИЛИ ССЫЛКИ СТАНОВЯТСЯ НАКЛЕЙКАМИ", rowId: `${usedPrefix + command} autosticker`},
+{title: "💬 | ТОЛЬКО ДЛЯ ПК", description: "БОТ БУДЕТ ОТВЕЧАТЬ НА КОМАНДЫ ТОЛЬКО В ТОМ СЛУЧАЕ ЕСЛИ ЭТО ПРИВАТНЫЙ ЧАТ", rowId: `${usedPrefix + command} pconly`},
+{title: "🏢 | ТОЛЬКОГРУППА", description: "БОТ БУДЕТ ОТВЕЧАТЬ НА КОМАНДЫ ТОЛЬКО ЕСЛИ ЭТО ГРУППА", rowId: `${usedPrefix + command} gconly`},]}, ]
 let name = await conn.getName(m.sender)
 const listMessage = {
 text: ' ',
 footer: `
-▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
-║➤ *✨𝐇𝐎𝐋𝐀, ${name}!!*
-▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+▬ ▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬ ▬
+║➤ *✨ПРИВЕТ, ${name}!!*
+▬ ▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬ ▬
 ┏━━━━━━━━━━━━━┓
 ┣❧ *𝚂𝙴𝙻𝙴𝙲𝙲𝙸𝙾𝙽𝙰 𝚄𝙽𝙰 𝙳𝙴 𝙻𝙰𝚂 𝙾𝙲𝙿𝙸𝙾𝙽𝙴𝚂 𝚀𝚄𝙴 𝚂𝙰𝙻𝙴𝙽 𝙴𝙽 𝙻𝙰 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 𝙻𝙸𝚂𝚃𝙰 𝙾 𝙳𝙰 𝙲𝙻𝙸𝙲𝙺 𝙴𝙽 𝙰𝙻𝙶𝚄𝙽 𝙱𝙾𝚃𝙾𝙽 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *welcome*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *welcome*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *public*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *public*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *antilink*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *antilink*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *antilink2*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *antilink2*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *detect*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *detect*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *restrict*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *restrict*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *pconly*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *pconly*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *gconly*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *gconly*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *autoread*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *autoread*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *audios*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *audios*_
-┣ ඬ⃟ℹ️ _${usedPrefix}enable *autosticker*_
-┣ ඬ⃟ℹ️ _${usedPrefix}disable *autosticker*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *приветствие*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *приветствие*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *общественный*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *общественный*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *антиссылка*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *антиссылка*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *антиссылка2*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *антиссылка2*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *обнаружить*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *обнаружить*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *ограничивать*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *ограничивать*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *толькодляПК*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *толькодляПК*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *толькоgc*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *толькоgc*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *авточтение*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *авточтение*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *аудио*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *аудио*_
+┣ ඬ⃟ℹ️ _${usedPrefix}включить  *автостикер*_
+┣ ඬ⃟ℹ️ _${usedPrefix}выключить *автостикер*_
 ┗━━━━━━━━━━━━━┛
 ${author}`,
 title: null,
-buttonText: "𝐒𝐄𝐋𝐄𝐂𝐂𝐈𝐎𝐍𝐄 𝐀𝐐𝐔𝐈",
+buttonText: "ВЫБЕРИТЕ ЗДЕСЬ",
 sections }
 
 let isEnable = /true|enable|(turn)?on|1/i.test(command)
@@ -60,7 +59,7 @@ let bot = global.db.data.settings[conn.user.jid] || {}
 let type = (args[0] || '').toLowerCase()
 let isAll = false, isUser = false
 switch (type) {
-case 'welcome':
+case 'приветствие':
 if (!m.isGroup) {
 if (!isOwner) {
 global.dfail('group', m, conn)
@@ -72,7 +71,7 @@ throw false
 }
 chat.welcome = isEnable
 break
-case 'detect':
+case 'обнаружить':
 if (!m.isGroup) {
 if (!isOwner) {
 global.dfail('group', m, conn)
@@ -84,7 +83,7 @@ throw false
 }
 chat.detect = isEnable
 break
-case 'delete':
+case 'удалить':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -92,7 +91,7 @@ throw false
 }}
 chat.delete = isEnable
 break
-case 'antidelete':
+case 'антиудаления':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -100,7 +99,7 @@ throw false
 }}
 chat.delete = !isEnable
 break
-case 'public':
+case 'общественный':
 isAll = true
 if (!isROwner) {
 global.dfail('rowner', m, conn)
@@ -108,7 +107,7 @@ throw false
 }
 global.opts['self'] = !isEnable
 break
-case 'antilink':
+case 'антиссылка':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -116,7 +115,7 @@ throw false
 }}
 chat.antiLink = isEnable
 break
-case 'antilink2':
+case 'антиссылка2':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -124,7 +123,7 @@ throw false
 }}
 chat.antiLink2 = isEnable          
 break
-case 'autosticker':
+case 'автостикер':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -132,7 +131,7 @@ throw false
 }}
 chat.autosticker = isEnable          
 break
-case 'audios':
+case 'аудио':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
@@ -140,7 +139,7 @@ throw false
 }}
 chat.audios = isEnable          
 break
-case 'restrict':
+case 'ограничивать':
 isAll = true
 if (!isOwner) {
 global.dfail('owner', m, conn)
@@ -156,7 +155,7 @@ throw false
 }
 global.opts['nyimak'] = isEnable
 break
-case 'autoread':
+case 'авточтение':
 isAll = true
 if (!isROwner) {
 global.dfail('rowner', m, conn)
@@ -164,7 +163,7 @@ throw false
 }
 global.opts['autoread'] = isEnable
 break
-case 'pconly':
+case 'толькодляПК':
 case 'privateonly':
 isAll = true
 if (!isROwner) {
@@ -173,7 +172,7 @@ throw false
 }
 global.opts['pconly'] = isEnable
 break
-case 'gconly':
+case 'толькогруппа':
 case 'grouponly':
 isAll = true
 if (!isROwner) {
@@ -195,11 +194,11 @@ default:
 if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, listMessage)
 throw false
 }
-conn.sendButton(m.chat, `🗂️ 𝐎𝐏𝐂𝐈𝐎𝐍: ${type} 
-🎚️ 𝐄𝐒𝐓𝐀𝐃𝐎: ${isEnable ? '𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾' : '𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾'}
-📣 𝐏𝐀𝐑𝐀: ${isAll ? '𝙴𝚂𝚃𝙴 𝙱𝙾𝚃' : isUser ? '' : '𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃'}`, author, null, [[`${isEnable ? '✖️ 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 ✖️' : '✔️ 𝙰𝙲𝚃𝙸𝚅𝙰𝚁 ✔️'}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`], ['👾 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 👾', '.menu']],m)}
+conn.sendButton(m.chat, `🗂️ ВЫБОР: ${type} 
+🎚️ КОМАНДА: ${isEnable ? 'ВКЛЮЧИНА' : 'ОТКЛЮЧИНА'}
+📣 ДЛЯ: ${isAll ? 'ЭТОТ БОТ' : isUser ? '' : 'ЭТОТ ЧАТ'}`, author, null, [[`${isEnable ? '✖️ ОТКЛЮЧИТЬ ✖️' : '✔️ ВКЛЮЧИТЬ ✔️'}`, `${isEnable ? `.off ${type}` : `.on ${type}`}`],],m)}
 
-handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.help = ['вкл', 'выкл',].map(v => v + 'ючить <option>')
 handler.tags = ['group', 'owner']
-handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
+handler.command = /^((вкл|выкл)ючить|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
 export default handler
