fd0d504aa52e440c7538e2b378156f6fca626ac0
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-30 21:54:48 +0300

    раапу

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -12,7 +12,7 @@ mentionedJid: [who]
 }})}
 handler.help = ['remove <@user>']
 handler.tags = ['owner']
-handler.command = /^(понизить|-|del)prem$/i
+handler.command = /^(снять|-|del)prem$/i
 handler.group = true
 handler.rowner = true
 export default handler
