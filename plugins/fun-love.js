a2e43147d9655f7eeb15d2cc3847e5042ca71c5d
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-30 19:18:51 +0300

    шохзщзол

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 conn.reply(m.chat, `
 *❤️❤️ ИЗМЕРИТЕЛЬ ЛЮБВИ ❤️❤️*
-*Любовь к тебе ${text} из* *${Math.floor(Math.random() * 100)}%* *de un 100%*
+*Любовь к тебе ${text}* *${Math.floor(Math.random(из) * 100)}%* *de un 100%*
 *Вы должны попросить руки и сержце (её\его)?* стать (вашей девушкой\вашем парнем)
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
