b02f4f49893ba9b65d84fbcf723b51d1db326a28
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-30 17:03:16 +0300

    ЛП

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -9,14 +9,14 @@ let isClose = { // Switch Case Like :v
 }[(args[0] || '')]
 if (isClose === undefined)
 throw `
-*[❗] 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙴𝚁𝚁𝙾𝙽𝙴𝙾!!*
+*[❗] ВЫБЕРИТЕ КОМАНДУ!!*
 
 *┏━━━❲ ✨𝙴𝙹𝙴𝙼𝙿𝙻𝙾✨ ❳━━━┓* 
 *┠┉↯ ${usedPrefix + command} открыть*
 *┠┉↯ ${usedPrefix + command} закрыть*
 `.trim()
 await conn.groupSettingUpdate(m.chat, isClose)
-{m.reply('*[ ✔ ] 𝙶𝚁𝚄𝙿𝙾 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*')}
+{m.reply('*[ ✔ ] КОМАНДА ВЫПОЛНЕНА*')}
 }
 handler.help = ['group открыть / закрыть', 'grupo abrir / cerrar']
 handler.tags = ['group']
