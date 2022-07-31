bc6eff0e01ddf3e1bfdded1265aeee37a637fd33
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-31 15:59:08 +0300

    апгнд

--- plugins/random-lolie.js
+++ plugins/random-lolie.js
@@ -9,5 +9,5 @@ let handler = async (m, { conn, text, command }) => {
   export default handler
   
   global.loli = [
-    "https://vk.com/video-36018360_456260552",
+    "https://youtu.be/6uHHWgJ6dsk",
   ]
