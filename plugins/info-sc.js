e8650aa54814acc455e73449a35f91873d987a19
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-07-31 19:51:30 +0300

    оне

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -26,5 +26,5 @@ let buttonMessage= {
 {buttonId: `${usedPrefix}donar`, buttonText: {displayText: ''}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
-handler.command = ['дайкишки','sc22']
+handler.command = ['дайкишки','sc']
 export default handler
