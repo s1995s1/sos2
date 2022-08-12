cd284fbf3bbd713edea89f5312231f6455c89d07
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-12 22:02:30 +0300

    ъззълл

--- plugins/nv-hentai.mp3.js
+++ plugins/nv-hentai.mp3.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/hentai.mp3'
+let vn = './media/привет.mp3'
 conn.sendFile(m.chat, vn, 'hentai.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true
 })
 }
-handler.customPrefix = /audio hentai|Audio hentai|audiohentai|Audiohentai/i
+handler.customPrefix = /Привет|привет/i
 handler.command = new RegExp
 export default handler
