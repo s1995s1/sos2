b2b0731baef2f12e5ddebafdb0e15efc58100317
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-12 20:22:24 +0300

    сммиим

--- plugins/nv-buenas noches_mp3.js
+++ plugins/nv-buenas noches_mp3.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/boanoite.mp3'
+let vn = './media/меню.mp3'
 conn.sendFile(m.chat, vn, 'boanoite.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /buenas noches|Buenas noches|Boanoite|boanoite/
+handler.customPrefix = /меню|Меню|Buenas noches|Boanoite|boanoite/
 handler.command = new RegExp
 export default handler
