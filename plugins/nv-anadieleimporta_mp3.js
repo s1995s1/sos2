96a1219df34064eb924c8970c54d7c5a45558777
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-12 20:51:13 +0300

    укл

--- plugins/nv-anadieleimporta_mp3.js
+++ plugins/nv-anadieleimporta_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/dylan1.mp3'
+let vn = './media/правила.mp3'
 conn.sendFile(m.chat, vn, 'dylan1.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /anadieleimporta|a nadie le importa/i
+handler.customPrefix = /правила|Правила/i
 handler.command = new RegExp
 
 handler.fail = null
