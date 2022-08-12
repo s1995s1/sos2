e7e6eee34f5bd9e3783e92f0c48aaf815b3d7300
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-12 20:32:29 +0300

    зжшзопимшзо

--- plugins/nv-feliz cumple_mp3.js
+++ plugins/nv-feliz cumple_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Feliz cumple.mp3'
+let vn = './media/новенький.mp3'
 conn.sendFile(m.chat, vn, 'Feliz cumple.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /feliz cumpleaños|felizcumpleaños|happy birthday/i
+handler.customPrefix = /новенький|Новенький|happy birthday/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
