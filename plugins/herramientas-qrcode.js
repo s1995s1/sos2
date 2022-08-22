6af877df81fcb2c578eb4ccbd92f1280f4bff5d1
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-22 22:57:56 +0300

    он

--- plugins/herramientas-qrcode.js
+++ plugins/herramientas-qrcode.js
@@ -3,7 +3,7 @@ let handler = async (m, { text, conn }) => {
 if (!text) throw `*[❗ИНФО❗] ВВЕДИТЕ ТЕКСТ КОТОРЫЙ ВЫ ХОТИТЕ ПРЕОБРОЗОВАТЬ В 𝚀𝚁-КОД*`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.help = ['текстовыйкод', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^текстовыйкод)?$/i
 export default handler
