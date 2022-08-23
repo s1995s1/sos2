5a668da8b52bfe22c0f88aacff6b4da31da65890
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-23 14:03:52 +0300

    .kgb

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -44,7 +44,7 @@ else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾�
 }}
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.command = /^(стикер|с)s(tic?ker)?(gif)?(wm)?$/i
 export default handler
 
 const isUrl = (text) => {
