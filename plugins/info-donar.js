2830f4025c77da3bb3e17b881aff9ef0297b4302
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-08-24 23:47:16 +0300

    анпль

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
 *┏ ┅ ━━━━━━━━━ ┅ ━*
-*┇          「 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼 」*
+*┇          「 Бот Царя 」*
 *┣ ┅ ━━━━━━━━━ ┅ ━*
-*┃ 𝙷𝙾𝙻𝙰 ${name} 💙*
+*┃ Привет ${name} 💙*
 *┃*
-*┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
-*┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝚂𝙴𝙶𝚄𝙸𝚁𝙼𝙴 :𝟹*
+*┃ 👉🏻 Хочешь снять бота?*
+*┃ Пиши моему создателю*
 *┃*
-┃ https://www.instagram.com/idk_jhuz/
+*┃Wa.me/+7910070455
 *┗ ┅ ━━━━━━━━━ ┅ ━*
 `.trim()
-conn.sendHydrated(m.chat, donar, wm, null, 'https://www.instagram.com/idk_jhuz/', '𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
+conn.sendHydrated(m.chat, donar, wm, null, 'https://www.instagram.com/idk_jhuz/', '', null, null, [['ОСНОВНОЕ МЕНЮ', '/menu']], m)}
 handler.help = ['donasi']
 handler.tags = ['info']
-handler.command = /^dona(te|si)|donar|apoyar$/i
+handler.command = /^dona(te|si)|аренда|apoyar$/i
 export default handler
