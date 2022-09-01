3808fcaca7e5f684deea914d2fe9ebedbc9a6297
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-02 00:11:42 +0300

    лгнна

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -11,7 +11,7 @@ await delay(1 * 1000)
 }}
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = /^(удалить|sacar|\-)$/i
+handler.command = /^(снести|sacar|\-)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
