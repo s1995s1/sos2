210614ad994aa2d9d6472b68ba8cf091cbe3d4ce
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-03 18:37:27 +0300

    орар

--- plugins/herramientas-qrcode.js
+++ plugins/herramientas-qrcode.js
@@ -5,5 +5,5 @@ conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcod
 }
 handler.help = ['код', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
-handler.command = /^код)?$/i
+handler.command = /^код?$/i
 export default handler
