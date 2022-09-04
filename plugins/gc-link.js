f25edd27839e7866ba1564dbdeb07128e1a1edc6
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-04 17:43:12 +0300

    vkghjhkl

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, args }) => {
 let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
+title: 'ССЫЛКА ГРУППЫ',
 body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+previewType: 0, thumbnail: fs.readFileSync("./src\admins.jpg"),
 sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
-handler.command = /^link(gro?up)?$/i
+handler.command = /^ссылка(gro?up)?$/i
 handler.group = true
 handler.botAdmin = true
 export default handler
