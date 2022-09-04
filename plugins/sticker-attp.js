f9fc1ec94fbe87a8894cc5301831d51972d0b44d
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-04 17:21:08 +0300

    sdrj

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
-handler.help = ['attp <teks>']
+handler.help = ['attp, <teks>']
 handler.tags = ['sticker']
-handler.command = /^стик$/i
+handler.command = /^attp$/i
 export default handler
