3da1da8666ba6174eb7c6143770ede933ad408ed
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-04 17:57:36 +0300

    ангншщ

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,10 +4,10 @@ let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'ССЫЛКА ГРУППЫ',
-body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
+body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',    
 previewType: 1, thumbnail: fs.readFileSync("./src/admins.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
-}
+sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}}) ('ссылка группы',) 
+} 
 handler.help = ['linkgroup']
 handler.tags = ['group']
 handler.command = /^ссылка(gro?up)?$/i
