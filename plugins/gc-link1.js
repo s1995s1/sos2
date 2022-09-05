57d07d8014b92715ad1050a13b75ad0ea471e09a
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-06 00:29:25 +0300

    юлгпгл

--- plugins/gc-link1.js
+++ plugins/gc-link1.js
@@ -3,12 +3,12 @@ let group = m.chat
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 //m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
   
-conn.sendHydrated(m.chat, ('https://chat.whatsapp.com будьте окуратны ссылкой' + await conn.groupInviteCode(group)), wm, pp, null, null, null, null, [
+conn.sendHydrated(m.chat, ('https://chat.whatsapp.com/' + await conn.groupInviteCode(group)), wm, pp, null, null, null, null, [
 ['ПОЛНОЕ ИМЕНЮ ☘️', '/menu']], m)
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
-handler.command = /^enlace|link(gro?up)?$/i
+handler.command = /^enlace|ссылка(gro?up)?$/i
 handler.group = true
 //handler.admin = false
 handler.botAdmin = true
