0091ce5356eb4f75aee76ac80fcf7ff24277df77
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-06 11:40:22 +0300

    нпглдгнп

--- plugins/gc-info.js
+++ plugins/gc-info.js
@@ -4,7 +4,6 @@ const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, 
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-let vn = './media/день.mp3'
 let text = `*「 ИНФОРМАЦЫЯ ГРУППЫ 」*\n
 *ИДЕНТИФИКАЦЫЯ ГРУППЫ:* 
 ${groupMetadata.id}
@@ -34,9 +33,6 @@ ${groupMetadata.id}
 `.trim()
 conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
-//await conn.sendFile(m.chat, vn, 'день.mp3', null, m, true, {
-//type: 'audioMessage', 
-//ptt: true})
 handler.help = ['infogrup']
 handler.tags = ['group']
 handler.command = /^(инфогруппы|gro?upinfo|info(gro?up|gc))$/i
