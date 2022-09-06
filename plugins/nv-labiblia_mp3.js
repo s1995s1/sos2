67ca956bf5ead0963e8da2831d8a18c840ed8d17
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-06 12:10:54 +0300

    апнл

--- plugins/nv-labiblia_mp3.js
+++ plugins/nv-labiblia_mp3.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/привет.mp3'
+let vn = './media/день.mp3'
 conn.sendFile(m.chat, vn, 'ora.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true
 })
 }
-handler.customPrefix = /|/
+handler.customPrefix = /какой сегодня день|Какой сегодня день/
 handler.command = new RegExp
 export default handler
