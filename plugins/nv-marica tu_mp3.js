8689ccd9131e4bd28461f9214f7d5e5c1f622410
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-06 12:24:50 +0300

    веверь

--- plugins/nv-marica tu_mp3.js
+++ plugins/nv-marica tu_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/cancion.mp3'
+let vn = './media/новость.mp3'
 conn.sendFile(m.chat, vn, 'cancion.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Marica tu|cancion1|Marica quien/i
+handler.customPrefix = /Новости|новости/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
