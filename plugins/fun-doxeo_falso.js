55e410d1b72892391145fbf1d3efbfeed4b5d7bb
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-07 23:08:00 +0300

    ЖДЛРИМГЛР

--- plugins/fun-doxeo_falso.js
+++ plugins/fun-doxeo_falso.js
@@ -57,7 +57,7 @@ conn.reply(m.chat, doxeo, m)
 }
 handler.help = ['doxear <nombre> | <@tag>']
 handler.tags = ['fun']
-handler.command = /^база|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i
+handler.command = /^листА|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i
 export default handler
 
 function pickRandom(list) {
