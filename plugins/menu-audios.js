03a3d6296bb910dff672b2e86042c4c6eae66d3e
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-17 00:47:51 +0300

    вкгнд

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -38,7 +38,7 @@ let str = `
 ° ඬ⃟🔊 _Я худею_
 ° ඬ⃟🔊 _Ага_
 `.trim()
-conn.sendHydrated(m.chat, str, wm, pp, '', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, '', 'Это что', null, null, [
 ['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
 ], m)
 }}
