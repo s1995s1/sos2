d1bea1eab7bac4988c843c429776d07c8bed7824
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-16 20:18:38 +0300

    лггр

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -38,7 +38,7 @@ let str = `
 ° ඬ⃟🔊 _Я худею_
 ° ඬ⃟🔊 _Ага_
 `.trim()
-conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, '', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
 ], m)
 }}
