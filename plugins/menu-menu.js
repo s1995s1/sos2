38909b45c6575370503cbdf53a284e77ee955014
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-16 20:39:06 +0300

    шдп

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) =
 try {
 let vn = './media/menu.mp3'
 let pp = './Menu2.jpg'
-let img = await(await fetch('Альтрон')).buffer()
+let img = await(await fetch('https://www.paidmembershipspro.com')).buffer()
 let d = new Date(new Date + 3600000)
 let locale = 'es'
 let week = d.toLocaleDateString(locale, { weekday: 'long' })
