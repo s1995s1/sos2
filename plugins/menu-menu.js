8ac4a8604f47848f767332999ee647b0cbd283c2
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-16 20:35:59 +0300

    ыпи

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) =
 try {
 let vn = './media/menu.mp3'
 let pp = './Menu2.jpg'
-let img = await(await fetch('https://www.paidmembershipspro.com/wp-content/uploads/2017/07/PayPal-Express.png')).buffer()
+let img = await(await fetch('Альтрон')).buffer()
 let d = new Date(new Date + 3600000)
 let locale = 'es'
 let week = d.toLocaleDateString(locale, { weekday: 'long' })
