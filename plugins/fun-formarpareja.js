c05435597f650ffe5825de621c404a8478f81495
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-20 20:39:10 +0300

    перевод

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -5,11 +5,11 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, null, {
+m.reply(`*${toM(a)}, ТЫ ДОЛЖЕН ЖЕНИТЬСЯ 💍 𝙲𝙾𝙽 ${toM(b)}, ЗДЕЛАЙ ЕЙ ПРЕДЛОЖЕНИЯ 💓*`, null, {
 mentions: [a, b]
 })}
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
-handler.command = ['ух','formarparejas']
+handler.command = ['свадьба','formarparejas']
 handler.group = true
 export default handler
