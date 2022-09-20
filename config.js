d882fca7433a12b984371f8978dfb87305a42700
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-20 12:39:25 +0300

    роршл

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 import fs from 'fs'
 
 global.owner = [
-  ['+79010070455', '👑 Mystic - Creador 👑', true],
+  ['79010070455', '👑 Альтрон - Царя 👑', true],
 ]
 
 global.packname = '(☞Альтронﾟ)☞'
