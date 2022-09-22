32e5053066de806af8a764f7ec0890eca2eff547
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-22 23:22:30 +0300

    аудио

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -92,7 +92,7 @@ let vn = './media/gemi2.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
-if (!chat.isBanned && chat.audios && m.text.match(/(Заебал|заебал)/gi)) {    
+if (!chat.isBanned && chat.audios && m.text.match(/(чистка|Чистка)/gi)) {    
 let vn = './media/досвидания.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
