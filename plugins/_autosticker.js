8dcf3088d348e250a9d30656fd2383999c8ed816
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-23 22:48:26 +0300

    пере

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -16,7 +16,7 @@ let img = await q.download?.()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗ИНФО❗] ВИДЕО МАКСИМАЛЬНОЕ ДЛЯ СТИКЕРА НЕ БОЛЬШЕ 7 СЕКУНД*', wm, [['ВЫКЛЮЧИТЬ АВТОСТИКЕР', '/выключить автостикер']], m)
 let img = await q.download()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
