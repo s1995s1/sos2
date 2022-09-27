f4c1f1b723208b0845c618a02f36dc6125be754c
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-27 23:05:05 +0300

    ОБНОВЛЕНИЕ

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
-m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n\n*—◉ 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚁𝙴𝙰𝙲𝙲𝙸𝙾𝙽𝙰𝚁𝙰 𝙰 𝙽𝙸𝙽𝙶𝚄𝙽 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙷𝙰𝚂𝚃𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃*')
+m.reply('*[❗ИНФО❗] ЭТОТ ЧАТ БЫЛ УСПЕШНО ЗАБЛОКИРОВАН*\n\n*—◉ БОТ НЕ БУДЕТ РЕАГИРОВАТЬ НЕ НА ОДНУ КОМАНДУ В ЭТОЙ ГРУППЕ ПОКА ЭТО ЧАТ ЗАБЛОКИРОВАН*')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
