24ea4c762127b95de1dab199b2105eb946101750
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-27 22:39:36 +0300

    добовления водео

--- plugins/adult-videoxxx.js
+++ plugins/adult-videoxxx.js
@@ -4,26 +4,26 @@ let res = pickRandom(asupan)
 conn.sendHydrated(m.chat, null, null, res, null, null, null, null, [['🔄 СЛЕДУЮЩАЯ 🔄', `/${command}`]], m)}
 handler.help = ['videoxxx']
 handler.tags = ['random']
-handler.command = /^videoxxx|vídeoxxx$/i
+handler.command = /^порновидео|vídeoxxx$/i
 export default handler
 
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
 
 const asupan = [
-"https://l.top4top.io/m_2235dduf01.mp4",
-"https://a.top4top.io/m_2235268m61.mp4",
-"https://b.top4top.io/m_2235k7hze2.mp4",
-"https://c.top4top.io/m_2235lxohb3.mp4",
-"https://d.top4top.io/m_2235jwd2e4.mp4",
-"https://e.top4top.io/m_2235h5b1z5.mp4",
-"https://f.top4top.io/m_2235gihcu6.mp4",
-"https://l.top4top.io/m_2235dp7m41.mp4",
-"https://a.top4top.io/m_2235zxue82.mp4",
-"https://b.top4top.io/m_2235m3bhf3.mp4",
-"https://c.top4top.io/m_2235vjyio4.mp4",
-"https://d.top4top.io/m_2235m9tdu5.mp4",
-"https://e.top4top.io/m_2235y2kon6.mp4",
+"https://cdn2.embed-player.space/21/11/16/1098605.mp4",
+"https://hot0.semyana.website/cached/828379.mp4",
+"https://hot.embed-player.space/cached/1096462.mp4",
+"https://cdn2.embed-player.space/19/09/26/736731.mp4",
+"https://cdn.embed-player.space/15/07/14/Dtr9iHiemU.mp4",
+"https://c-4.getw6.live/g2/nfpJb0MGweQuiYl9h0qdxXB6NG9Z9ERrLgVSqMuJNYSOMyUAgTOCKl7SOdglFWf1XTzXJH0QMnnlT3r20U7sKZrORKI12OJtJD1v5ZNmqBINm9UwOFxGYrgbZMU5pDC5IO%252FQgoApsy3xSA7WDXiDfo7U0lDYPFrBt%252Bfzq1lrw7I%253D.mp4",
+"https://store-3.getw6.live/g/R9e2SJWSHDv9kKN9JXGbPrF%252BuLuC6trX7z56AF%252BoXVbmP0slvYtUOetDnpmBl7dFDMxzLZ%252Fyklc8U9MnlbN6FxvcJB1A88%252BYqjKkb7Uneo9V5iYLCkdqdUJPeDYMwsDJzqQ3BrS8q%252BP32Z00F%252FdTgpro0fLYpvy5Kkg9g0R%252BTv0%253D.mp4",
+"https://store-3.getw6.live/g/LPBVH5i0RnoyYXrCp5Gl7HwHIrvLCy60qMf%252FTr%252B%252BwTPq9VXnxio0Hhedf3TjR8F92XQTvQKTKn30O3ey7fp%252BWv8u0IDV9lxft6VP%252B4uEUeKsWg6uEgOsvJRhpm6ZIe1Kmn0Q5AXzEd1KkZV1alGcBzXnFY1UoXBYIGLPNayVshw%253D.mp4",
+"https://store-3.getw6.live/g/aiZ1dyWf9jhVye3E14EG7OAIwLldXdOJRzoP72WyrdwwIODIopgamjxhuxvxHP%252FVmQfrwkKzlcm0EiBLWj6mYRa6VC4gCxGug0%252F4yECITIvHTYA5IrJvyzXTPmLd68XDqaUBCWfoUNYXWn1UlL1q%252BXL4KmBTjgpaWej%252FdhwCOeo%253D.mp4",
+"https://c-4.getw6.live/g2/aY21uOcUB6SzhctY9SOOWAsBdXPpxxKziTkFBcYBC6dRV%252B%252Bqdlr9jQJxGtf78gi0qrdLBbdrR38EMMaEJi%252BySU4FNJOGSWrb0uUzxkK4DYrO8RgXiR5B6OsH5YopInVdj3o37dgAwTENsq9Bh8eFdehe2Pm1gc7vgNUgUcJqHAU%253D.mp4",
+"https://c-4.getw6.live/g2/ZP68uzSo3M5Hsw0yaxVMZest4SuSm566nNHpJsv3blOfjbsQ9LlQk6T0QeFkCIn0qmwq3imbrc6lRcfNjXvbJHQWTcCezr2543gBvcNB31d3j9C%252BFC2DTfGoplIuW7Kd5oQynIgB0JgcqOop9OM5XoTJTVcbgPw%252FenZDF2kEdUg%253D.mp4",
+"https://store-3.getw6.live/g/AGdLL6gCzVP3SGpAJTKYxa9F6qu5dk6dHYSmpQFGb0pE9pCapEH9xxo22vpgVs5wmHz3Su7qgM5FLOyQzvUvYa8AcxtKRs%252BKogkKPthxZ6C3bDZeI%252BZxyzR5KQ%252BJgaQ%252B8ykVhf9wZsg%252FQXK9fiJY5RJvhrvpFbdxwbJ2W5P8TRY%253D.mp4",
+"https://c-4.getw6.live/g2/g9kekkT9Z%252Bg9bczl6nODTF3l7T2AgIfMAPDYzd3JeYUtLUV%252FtM%252FSPUyt%252BCHd1nqzTVG3axN4QetRhE6nFMqUaF63VpmAt2cNVtxtyIT3%252F613L%252BWyB9fKsYC2Tqc7uyxHuM0zDarHDdgNsc%252FJ3HDtb9whBDivuhN5ejioGt4o%252BSU%253D.mp4",
 "https://f.top4top.io/m_2235rhid57.mp4",
 "https://g.top4top.io/m_2235zgsqf8.mp4",
 "https://i.top4top.io/m_2235drxxg10.mp4",
