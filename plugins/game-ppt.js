a7ce91ca06f260d2a4c9c6ade891213d88867b2c
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-09-29 17:30:43 +0300

    перевод

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, command, usedPrefix, args }) => {
 let pp = 'https://www.bighero6challenge.com/images/thumbs/Piedra,-papel-o-tijera-0003318_1584.jpeg'
 if (!args[0]) throw conn.sendHydrated(m.chat, 'КАМЕНЬ, БУМАГА, ИЛИ НОЖНИЦЫ\n\Используйте КНОПКИ для игры ', wm, pp, null, null, null, null, [
-['Камень 🥌', `${usedPrefix + command} piedra`],
+['Камень 🥌', `${usedPrefix + command} камень`],
 ['Бумага 📄', `${usedPrefix + command} papel`],
 ['Ножницы ✂️', `${usedPrefix + command} tijera`]
 ], m)
 var astro = Math.random()
 if (astro < 0.34) {
-astro = 'piedra' 
+astro = 'камень' 
 } else if (astro > 0.34 && astro < 0.67) {
 astro = 'tijera' 
 } else {
@@ -15,9 +15,9 @@ astro = 'papel'
 }
 if (text == astro) {
 global.db.data.users[m.sender].exp += 500
-m.reply(`🔰 Empate!\n\n*👉🏻 Tu: ${text}\n👉🏻 Бот: ${astro}*`)
+m.reply(`🔰 Empate!\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
 } else if (text == 'papel') {
-if (astro == 'piedra') {
+if (astro == 'камень') {
 global.db.data.users[m.sender].exp += 1000
 m.reply(`🥳 Победа за Вами! 🎉\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
 } else {
@@ -41,14 +41,14 @@ global.db.data.users[m.sender].exp -= 300
 m.reply(`☠️ Вы проиграли! ❌\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
 }
 } else if (text == 'papel') {
-if (astro == 'piedra') {
+if (astro == 'камень') {
 global.db.data.users[m.sender].exp += 1000
 m.reply(`🥳 Победа за Вами! 🎉\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
 m.reply(`☠️ Вы проиграли! ❌\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
 }
-} else if (text == 'piedra') {
+} else if (text == 'камень') {
 if (astro == 'tijera') {
 global.db.data.users[m.sender].exp += 1000
 m.reply(`🥳 Победа за Вами! 🎉\n\n*👉🏻 Вы: ${text}\n👉🏻 Бот: ${astro}*`)
