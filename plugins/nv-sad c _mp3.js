bbcec2bd6b7f48d0c29d94ad4c23fed24114704f
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-10-03 23:41:16 +0300

    аудио

--- plugins/nv-sad c _mp3.js
+++ plugins/nv-sad c _mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/toma.mp3'
+let vn = './media/скучаю.mp3'
 conn.sendFile(m.chat, vn, 'toma.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.command = /^(:c|c)$/i
+handler.command = /^(скучаю|Скучаю)$/i
 handler.fail = null
 handler.exp = 100
 export default handler
