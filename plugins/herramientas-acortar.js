bff30efc6603d16aeec4f52079fc02b789778697
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-10-06 21:53:05 +0300

    СТРОКА

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, xteamkey }) => {
 if (!text) throw '*[❗ИНФО❗] ВВЕДИТЕ ССЫЛКУ КОТОРУЮ ВЫ ХОТИТЕ СОКРАТИТЬ'
 let json = await (await fetch(`https://api.xteam.xyz/shorturl/tinyurl?url=${text}&apikey=cb15ed422c71a2fb`)).json()
 if (!json.status) throw json
-let hasil = `*ВАША ССЫЛКА СОКРОЩЕНА!!*\n\n*ПРЕДЫДУЩАЯ ССЫЛКА:*\n${text}\n*СОКРАЩЕННАЯ ССЫЛКА:*\n\n*${json.result}*`.trim()   
+let hasil = `*ВАША ССЫЛКА СОКРОЩЕНА!!*\n\n\n*ПРЕДЫДУЩАЯ ССЫЛКА:*\n\n${text}\n\n*СОКРАЩЕННАЯ ССЫЛКА:*\n\n*${json.result}*`.trim()   
 m.reply(hasil)
 }
 handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
