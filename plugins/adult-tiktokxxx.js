8546445d5de2ba8be4bd7fab58ad2288646ba9b6
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-11-01 18:34:32 +0300

    обнова

--- plugins/adult-tiktokxxx.js
+++ plugins/adult-tiktokxxx.js
@@ -14,7 +14,7 @@ let json = await fetch(`http://mkbot.online/api/tiktok/nsfw/nsfwtt?&apikey=${mkb
 let jsons = await json.json()
 let res = jsons.result
 conn.sendButton(m.chat, `- 𝙱𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍𝚘 𝚊 𝚃𝚒𝚔𝚃𝚘𝚔 𝙶𝚘𝚕𝚍🥵 -`, `*◈•@${who.split("@s.whatsapp.net")[0]}*`, res, [['🔄 СЛЕДУЮЩАЯ 🔄', `/${command}`]], fgif, { mentions: [who] })}
-handler.command = /^(tiktokxxx)$/i
+handler.command = /^(видео)$/i
 export default handler
 global.mkbot = ['lwSUluWz', 'HSxJ72Rf', 'H6pvVPJc', 'uVAf54xz', 'nwJVzP6v']
 global.mkbotkey = mkbot[Math.floor(mkbot.length * Math.random())]
