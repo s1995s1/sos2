f1f863be107b77a63417efa818e46e4a4ce75ab1
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-11-01 17:37:38 +0300

    обнова

--- plugins/adult-video.js
+++ plugins/adult-video.js
@@ -2,15 +2,20 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'   
 let res = pickRandom(asupan)
 conn.sendHydrated(m.chat, null, null, res, null, null, null, null, [['🔄 СЛЕДУЮЩАЯ 🔄', `/${command}`]], m)}
-handler.help = ['порно2']
+handler.help = ['девушки']
 handler.tags = ['random']
-handler.command = /^порно2$/i
+handler.command = /^девушки$/i
 export default handler
 
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
 
 const asupan = [
-"",
-"",
+"https://disk.yandex.ru/i/wni8F73l6NlCGw",
+"https://disk.yandex.ru/i/KfgL_Iqg7wdQCg",
+"https://disk.yandex.ru/i/k0FodOnTtYSHMg",
+"https://disk.yandex.ru/i/CI7whKqGDGiclQ",
+"https://disk.yandex.ru/i/U_jmcG1DmKlhLg",
+"https://disk.yandex.ru/i/_6SqZ5-YEko6Zw",
+"https://disk.yandex.ru/i/SSF2wqHbTntvmQ",
 ]
