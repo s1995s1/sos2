08ea0154c4f79eaf9a67d91820ac7b7a6811700a
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-11-07 22:42:38 +0300

    hftfjk

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -13,7 +13,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 const linkThisGroup2 = `https://www.youtube.com/`
 const linkThisGroup3 = `https://youtu.be/`
-const linkThisGroup4 = `https://`
+const linkThisGroup4 = `https://r./`
 if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0
