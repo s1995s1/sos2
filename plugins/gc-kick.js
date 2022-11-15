271affd1786139198c58967eb6950f4f04295890
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-11-15 10:50:34 +0300

    обнова

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -7,6 +7,7 @@ let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')}
 handler.command = /^(снести|echar|hechar|sacar)$/i
 handler.admin = true
+handler.rowner = true
 handler.group = true
 handler.botAdmin = true
 export default handler
