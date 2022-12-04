080c30cebbc42ad8efa9ef945b22f17eee5635ef
s1995s1 <109981411+s1995s1@users.noreply.github.com>
2022-12-04 23:00:34 +0300

    лрподорп

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -67,11 +67,6 @@ let vn = './media/скучно.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
-if (!chat.isBanned && chat.audios && m.text.match(/(Добрый вечер|добрый вечер)/gi)) {    
-let vn = './media/добрый.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
-    
 if (!chat.isBanned && chat.audios && m.text.match(/(Брат|брат)/gi)) {    
 let vn = './media/брат.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
